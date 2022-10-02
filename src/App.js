ae083fdfaf4373106dfeea3c70c96754093a4dbe
Eric <ettrudell17@gmail.com>
2022-10-02 15:35:12 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Person from './components/Person';
+
+function App() {
+  return (
+    <div className="App">
+      <Person/>
+      <Person/>
+      <Person/>
+      <Person/>
+    </div>
+  );
+}
+
+export default App;
